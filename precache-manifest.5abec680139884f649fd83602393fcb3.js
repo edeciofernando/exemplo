5534ce5baf48eb346619f758369cb06332aff6b5
Edecio Fernando <edeciofernando@gmail.com>
2019-10-21 20:23:40 -0300

    Updates

---
+++ precache-manifest.5abec680139884f649fd83602393fcb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "029eacdae4f1a5bc45e5925257dc967a",
+    "url": "/exemplo/index.html"
+  },
+  {
+    "revision": "17ee5f31930a03056ee9",
+    "url": "/exemplo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0c16eddbbbef33f05157",
+    "url": "/exemplo/static/js/2.6dddde6a.chunk.js"
+  },
+  {
+    "revision": "17ee5f31930a03056ee9",
+    "url": "/exemplo/static/js/main.18ab605e.chunk.js"
+  },
+  {
+    "revision": "542a9a9572b001c97337",
+    "url": "/exemplo/static/js/runtime-main.4d574d48.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/exemplo/static/media/logo.25bf045c.svg"
+  }
+]);
